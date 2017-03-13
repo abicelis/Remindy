39ccb2cbaa4a94f324d528c04854cdc639de1b07
abicelis <abicelis@gmail.com>
2017-03-13 11:39:42 -0400

    List_item_reminder layout for viewHolder

---
+++ app/src/main/java/ve/com/abicelis/remindy/app/holders/ReminderViewHolder.java
@@ -0,0 +1,24 @@
+package ve.com.abicelis.remindy.app.holders;
+
+import android.app.Activity;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+
+/**
+ * Created by abice on 13/3/2017.
+ */
+
+public class ReminderViewHolder extends RecyclerView.ViewHolder {
+
+    public ReminderAdapter mAdapter;
+    private Activity mFragment;
+
+
+    public ReminderViewHolder(View itemView) {
+        super(itemView);
+    }
+
+
+
+
+}
