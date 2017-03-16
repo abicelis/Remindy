98758d75ca7f968c1f7f3310654bce5312d2392f
abicelis <abicelis@gmail.com>
2017-03-16 12:18:31 -0400

    Updated contract with SimpleReminder tables, refactored old tables to AdvancedReminder tables

--- app/src/main/java/ve/com/abicelis/remindy/database/RemindyContract.java
+++ app/src/main/java/ve/com/abicelis/remindy/database/RemindyContract.java
@@ -23,7 +23,9 @@ final class RemindyContract {
         static final TableColumn COLUMN_NAME_IS_ONE_OFF = new TableColumn(DataType.TEXT, "is_one_off");
     }
 
-    /* Reminder Table */
+
+
+    /* Advanced Reminder Table */
     static abstract class AdvancedReminderTable implements BaseColumns {
         static final String TABLE_NAME = "reminder_advanced";
 
@@ -31,22 +33,55 @@ final class RemindyContract {
         static final TableColumn COLUMN_NAME_TITLE = new TableColumn(DataType.TEXT, "title");
         static final TableColumn COLUMN_NAME_DESCRIPTION = new TableColumn(DataType.TEXT, "description");
         static final TableColumn COLUMN_NAME_CATEGORY = new TableColumn(DataType.TEXT, "category");
+
         static final TableColumn COLUMN_NAME_PLACE_FK = new TableColumn(DataType.TEXT, "fk_place");
-        static final TableColumn COLUMN_NAME_DATE_TYPE = new TableColumn(DataType.TEXT, "datetype");
-        static final TableColumn COLUMN_NAME_START_DATE = new TableColumn(DataType.INTEGER, "startdate");
-        static final TableColumn COLUMN_NAME_END_DATE = new TableColumn(DataType.INTEGER, "enddate");
-        static final TableColumn COLUMN_NAME_TIME_TYPE = new TableColumn(DataType.TEXT, "timetype");
-        static final TableColumn COLUMN_NAME_START_TIME = new TableColumn(DataType.INTEGER, "starttime");
-        static final TableColumn COLUMN_NAME_END_TIME = new TableColumn(DataType.INTEGER, "endtime");
+        static final TableColumn COLUMN_NAME_DATE_TYPE = new TableColumn(DataType.TEXT, "date_type");
+        static final TableColumn COLUMN_NAME_START_DATE = new TableColumn(DataType.INTEGER, "start_date");
+        static final TableColumn COLUMN_NAME_END_DATE = new TableColumn(DataType.INTEGER, "end_date");
+        static final TableColumn COLUMN_NAME_TIME_TYPE = new TableColumn(DataType.TEXT, "time_type");
+        static final TableColumn COLUMN_NAME_START_TIME = new TableColumn(DataType.INTEGER, "start_time");
+        static final TableColumn COLUMN_NAME_END_TIME = new TableColumn(DataType.INTEGER, "end_time");
+        //TODO: Weekday is missing here!
     }
 
-    /* ExtraText Table */
-    static abstract class ExtraTable implements BaseColumns {
-        static final String TABLE_NAME = "extra";
+    /* Extra Table for Advanced Reminders */
+    static abstract class AdvancedReminderExtraTable implements BaseColumns {
+        static final String TABLE_NAME = "reminder_advanced_extra";
 
         static final TableColumn COLUMN_NAME_REMINDER_FK = new TableColumn(DataType.TEXT, "fk_reminder");
         static final TableColumn COLUMN_NAME_TYPE = new TableColumn(DataType.TEXT, "type");
         static final TableColumn COLUMN_NAME_CONTENT_TEXT = new TableColumn(DataType.TEXT, "text_content");
         static final TableColumn COLUMN_NAME_CONTENT_BLOB = new TableColumn(DataType.BLOB, "blob_content");
     }
+
+
+
+    /* Simple Reminder Table */
+    static abstract class SimpleReminderTable implements BaseColumns {
+        static final String TABLE_NAME = "reminder_simple";
+
+        static final TableColumn COLUMN_NAME_STATUS = new TableColumn(DataType.TEXT, "status");
+        static final TableColumn COLUMN_NAME_TITLE = new TableColumn(DataType.TEXT, "title");
+        static final TableColumn COLUMN_NAME_DESCRIPTION = new TableColumn(DataType.TEXT, "description");
+        static final TableColumn COLUMN_NAME_CATEGORY = new TableColumn(DataType.TEXT, "category");
+
+        static final TableColumn COLUMN_NAME_DATE = new TableColumn(DataType.INTEGER, "date");
+        static final TableColumn COLUMN_NAME_TIME = new TableColumn(DataType.INTEGER, "time");
+        static final TableColumn COLUMN_NAME_REPEAT_TYPE = new TableColumn(DataType.TEXT, "repeat_type");
+        static final TableColumn COLUMN_NAME_REPEAT_INTERVAL = new TableColumn(DataType.INTEGER, "repeat_interval");
+        static final TableColumn COLUMN_NAME_REPEAT_END_TYPE = new TableColumn(DataType.TEXT, "repeat_end_type");
+        static final TableColumn COLUMN_NAME_REPEAT_END_NUMBER_OF_EVENTS = new TableColumn(DataType.INTEGER, "repeat_number_of_events");
+        static final TableColumn COLUMN_NAME_REPEAT_END_DATE = new TableColumn(DataType.INTEGER, "repeat_end_date");
+    }
+
+    /* Extra Table for Advanced Reminders */
+    static abstract class SimpleReminderExtraTable implements BaseColumns {
+        static final String TABLE_NAME = "reminder_simple_extra";
+
+        static final TableColumn COLUMN_NAME_REMINDER_FK = new TableColumn(DataType.TEXT, "fk_reminder");
+        static final TableColumn COLUMN_NAME_TYPE = new TableColumn(DataType.TEXT, "type");
+        static final TableColumn COLUMN_NAME_CONTENT_TEXT = new TableColumn(DataType.TEXT, "text_content");
+        static final TableColumn COLUMN_NAME_CONTENT_BLOB = new TableColumn(DataType.BLOB, "blob_content");
+    }
+
 }
