238517dd9f18cfca3497e2c590db1fd3cd9d8890
abicelis <abicelis@gmail.com>
2017-03-16 10:07:34 -0400

    ReminderRepeatType enum for SimpleReminder

---
+++ app/src/main/java/ve/com/abicelis/remindy/enums/ReminderRepeatType.java
@@ -0,0 +1,13 @@
+package ve.com.abicelis.remindy.enums;
+
+/**
+ * Created by abice on 16/3/2017.
+ */
+
+public enum ReminderRepeatType {
+    DISABLED,
+    DAILY,
+    WEEKLY,
+    MONTHLY,
+    YEARLY;
+}
