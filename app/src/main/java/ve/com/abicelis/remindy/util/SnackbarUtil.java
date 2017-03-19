a160bb45b0986ccb01b2e3b1c05e681ab9610210
abicelis <abicelis@gmail.com>
2017-03-19 17:17:53 -0400

    Improved SnackbarUtil

--- app/src/main/java/ve/com/abicelis/remindy/util/SnackbarUtil.java
+++ app/src/main/java/ve/com/abicelis/remindy/util/SnackbarUtil.java
@@ -1,10 +1,12 @@
 package ve.com.abicelis.remindy.util;
 
-import android.content.Context;
 import android.support.annotation.ColorRes;
+import android.support.annotation.DrawableRes;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.annotation.StringRes;
+import android.support.design.widget.BaseTransientBottomBar;
 import android.support.design.widget.Snackbar;
-import android.support.v4.content.ContextCompat;
 import android.view.Gravity;
 import android.view.View;
 import android.widget.TextView;
@@ -17,25 +19,62 @@ import ve.com.abicelis.remindy.R;
 
 public class SnackbarUtil {
 
-    public static void showErrorSnackbar(View container, @StringRes int textStringRes) {
-        doShowSnackbar(container, textStringRes, R.color.snackbar_error_background);
-    }
 
-    public static void showNoticeSnackbar(View container, @StringRes int textStringRes) {
-        doShowSnackbar(container, textStringRes, R.color.snackbar_notice_background);
+    public static void showSnackbar(View container, @NonNull SnackbarType snackbarType,
+                                    @StringRes int textStringRes, @Nullable SnackbarDuration duration,
+                                    @Nullable BaseTransientBottomBar.BaseCallback<Snackbar> callback) {
 
-    }
+        duration = (duration == null ? SnackbarDuration.LONG : duration);
 
-    public static void showSuccessSnackbar(View container, @StringRes int textStringRes) {
-        doShowSnackbar(container, textStringRes, R.color.snackbar_success_background);
-    }
-
-    private static void doShowSnackbar(View container, @StringRes int stringRes, @ColorRes int backgroundColorRes) {
-        Snackbar snackbar = Snackbar.make(container, stringRes, Snackbar.LENGTH_LONG);
-        snackbar.getView().setBackgroundResource(backgroundColorRes);
+        Snackbar snackbar = Snackbar.make(container, textStringRes, duration.getDuration());
+        snackbar.getView().setBackgroundResource(snackbarType.getColorRes());
         TextView snackbarText = (TextView) snackbar.getView().findViewById(android.support.design.R.id.snackbar_text);
-        snackbarText.setCompoundDrawablesWithIntrinsicBounds(0, 0, R.drawable.icon_error_snackbar, 0);
+        snackbarText.setCompoundDrawablesWithIntrinsicBounds(0, 0,snackbarType.getIconRes(), 0);
         snackbarText.setGravity(Gravity.CENTER);
+        if(callback != null)
+            snackbar.addCallback(callback);
         snackbar.show();
     }
+
+
+    public enum SnackbarDuration {
+        LONG(Snackbar.LENGTH_LONG),
+        SHORT(Snackbar.LENGTH_SHORT),
+        INDEFINITE(Snackbar.LENGTH_INDEFINITE);
+
+        private int duration;
+
+        SnackbarDuration(int duration){
+            this.duration = duration;
+        }
+
+        public int getDuration() {
+            return duration;
+        }
+    }
+
+    public enum SnackbarType {
+        ERROR(R.color.snackbar_error_background, R.drawable.icon_error_snackbar),
+        SUCCESS(R.color.snackbar_success_background, R.drawable.icon_success_snackbar),
+        NOTICE(R.color.snackbar_notice_background, R.drawable.icon_error_snackbar);
+
+        private @ColorRes int colorRes;
+        private @DrawableRes int iconRes;
+
+        SnackbarType(@ColorRes int colorRes, @DrawableRes int iconRes){
+            this.colorRes = colorRes;
+            this.iconRes = iconRes;
+        }
+
+        public @ColorRes int getColorRes() {
+            return colorRes;
+        }
+
+        public @DrawableRes int getIconRes() {
+            return iconRes;
+        }
+    }
+
 }
+
+
