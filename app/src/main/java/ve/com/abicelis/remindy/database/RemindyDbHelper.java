a531082b82861e7561b1ce6b69999de0df7b7696
abicelis <abicelis@gmail.com>
2017-03-27 19:32:01 -0400

    insertMockData() refactor

--- app/src/main/java/ve/com/abicelis/remindy/database/RemindyDbHelper.java
+++ app/src/main/java/ve/com/abicelis/remindy/database/RemindyDbHelper.java
@@ -12,7 +12,7 @@ import java.io.IOException;
 import java.util.Calendar;
 
 import ve.com.abicelis.remindy.model.Time;
-import ve.com.abicelis.remindy.util.FileUtils;
+import ve.com.abicelis.remindy.util.FileUtil;
 
 
 /**
@@ -67,7 +67,7 @@ public class RemindyDbHelper extends SQLiteOpenHelper {
         File backupDatabase = new File(mDbExternalBackupFilepath);
 
         if (appDatabase.exists()) {
-            FileUtils.copyFile(new FileInputStream(appDatabase), new FileOutputStream(backupDatabase));
+            FileUtil.copyFile(new FileInputStream(appDatabase), new FileOutputStream(backupDatabase));
             return true;
         }
         return false;
@@ -86,7 +86,7 @@ public class RemindyDbHelper extends SQLiteOpenHelper {
         File backupDatabase = new File(mDbExternalBackupFilepath);
 
         if (backupDatabase.exists()) {
-            FileUtils.copyFile(new FileInputStream(backupDatabase), new FileOutputStream(appDatabase));
+            FileUtil.copyFile(new FileInputStream(backupDatabase), new FileOutputStream(appDatabase));
             // Access the copied database so SQLiteHelper will cache it and mark
             // it as created.
             getWritableDatabase().close();
@@ -114,17 +114,34 @@ public class RemindyDbHelper extends SQLiteOpenHelper {
 
         cal.add(Calendar.DAY_OF_MONTH, -7);
         long dateLastWeek = cal.getTimeInMillis();
-        cal.add(Calendar.DAY_OF_MONTH, +9);
+
+        cal.add(Calendar.DAY_OF_MONTH, +6);
         long dateYesterday = cal.getTimeInMillis();
-        cal.add(Calendar.DAY_OF_MONTH, +5);
+
+        cal.add(Calendar.DAY_OF_MONTH, +1);
         long dateToday = cal.getTimeInMillis();
+
         cal.add(Calendar.DAY_OF_MONTH, +1);
         long dateTomorrow = cal.getTimeInMillis();
-        cal.add(Calendar.DAY_OF_MONTH, +8);
+
+        cal.add(Calendar.DAY_OF_MONTH, +1);
+        long dateIn2Days = cal.getTimeInMillis();
+
+        cal.add(Calendar.DAY_OF_MONTH, +6);
         long dateNextWeek = cal.getTimeInMillis();
 
+        cal.add(Calendar.DAY_OF_MONTH, -8);
+        cal.add(Calendar.MONTH, +1);
+        long dateNextMonth = cal.getTimeInMillis();
 
+        cal.add(Calendar.MONTH, +2);
+        long dateNext3Months = cal.getTimeInMillis();
 
+        cal.add(Calendar.YEAR, +1);
+        long dateNextYear = cal.getTimeInMillis();
+
+        cal.add(Calendar.YEAR, +1);
+        long dateFuture = cal.getTimeInMillis();
 
         //Insert mock Tasks Table
         statement = "INSERT INTO " + RemindyContract.TaskTable.TABLE_NAME + " (" +
@@ -136,20 +153,32 @@ public class RemindyDbHelper extends SQLiteOpenHelper {
                 RemindyContract.TaskTable.COLUMN_NAME_REMINDER_TYPE.getName() + COMMA_SEP +
                 RemindyContract.TaskTable.COLUMN_NAME_DONE_DATE.getName() +
                 ") VALUES " +
-                "(0,    'UNPROGRAMMED',     'Mock Task 2',      'Task 2 - No Reminder',          'PERSONAL', 'NONE',             -1)," +
-                "(1,    'UNPROGRAMMED',     'Mock Task 3',      'Task 3 - No Reminder',          'BUSINESS', 'NONE',             -1)," +
-                "(2,    'UNPROGRAMMED',     'Mock Task 1',      'Task 1 - No Reminder',          'PERSONAL', 'NONE',             -1)," +
-                "(3,    'UNPROGRAMMED',     'Mock Task 4',      'Task 4 - No Reminder',          'PERSONAL', 'NONE',             -1)," +
-                "(4,    'PROGRAMMED',        'Mock Task 5',      'Task 5 - One-time Reminder',    'PERSONAL', 'ONE_TIME',         -1)," +
-                "(5,    'PROGRAMMED',        'Mock Task 6',      'Task 6 - One-time Reminder',    'BUSINESS', 'ONE_TIME',         -1)," +
-                "(6,    'PROGRAMMED',        'Mock Task 7',      'Task 7 - Repeating Reminder',   'PERSONAL', 'REPEATING',        -1)," +
-                "(7,    'PROGRAMMED',        'Mock Task 8',      'Task 8 - Location Reminder',    'BUSINESS', 'LOCATION_BASED',   -1)," +
-                "(8,    'PROGRAMMED',        'Mock Task 9',      'Task 9 - Location Reminder',    'PERSONAL', 'LOCATION_BASED',   -1)," +
-                "(9,    'PROGRAMMED',        'Mock Task 10',     'Task 10 - Repeating Reminder',  'BUSINESS', 'REPEATING',        -1)," +
-                "(10,   'PROGRAMMED',        'Mock Task 11',     'Task 11 - Repeating Reminder',  'BUSINESS', 'REPEATING',        -1)," +
-                "(11,   'DONE',             'Mock Task 12',     'Task 12 - One-time DONE',       'PERSONAL', 'ONE_TIME',         "+dateToday+")," +
-                "(12,   'DONE',             'Mock Task 13',     'Task 13 - One-time DONE',       'BUSINESS', 'ONE_TIME',         "+dateYesterday+")," +
-                "(13,   'DONE',             'Mock Task 14',     'Task 14 - One-time DONE',       'PERSONAL', 'ONE_TIME',         "+dateLastWeek+");";
+                "(0,    'UNPROGRAMMED',     'Mock Task 0',      'Task 0 - No Reminder',             'PERSONAL',    'NONE',             -1)," +
+                "(1,    'UNPROGRAMMED',     'Mock Task 1',      'Task 1 - No Reminder',             'BUSINESS',    'NONE',             -1)," +
+                "(2,    'UNPROGRAMMED',     'Mock Task 2',      'Task 2 - No Reminder',             'PERSONAL',    'NONE',             -1)," +
+                "(3,    'UNPROGRAMMED',     'Mock Task 3',      'Task 3 - No Reminder',             'PERSONAL',    'NONE',             -1)," +
+
+
+                "(4,    'DONE',             'Mock Task 4',      'Task 4 - One-time DONE',           'PERSONAL',     'ONE_TIME',         "+dateToday+")," +
+                "(5,    'DONE',             'Mock Task 5',      'Task 5 - One-time DONE',           'BUSINESS',     'ONE_TIME',         "+dateYesterday+")," +
+                "(6,    'DONE',             'Mock Task 6',      'Task 6 - One-time DONE',           'PERSONAL',     'ONE_TIME',         "+dateLastWeek+")," +
+                "(7,    'DONE',             'Mock Task 7',      'Task 7 - One-time DONE',           'BUSINESS',     'ONE_TIME',         "+dateToday+")," +
+                "(8,    'DONE',             'Mock Task 8',      'Task 8 - One-time DONE',           'PERSONAL',     'ONE_TIME',         "+dateYesterday+")," +
+                "(9,    'DONE',             'Mock Task 9',      'Task 9 - One-time DONE',           'BUSINESS',     'ONE_TIME',         "+dateLastWeek+")," +
+
+                "(10,   'PROGRAMMED',       'Mock Task 10',     'Task 10 - One-time Reminder',      'PERSONAL',     'ONE_TIME',         -1)," +
+                "(11,   'PROGRAMMED',       'Mock Task 11',     'Task 11 - One-time Reminder',      'BUSINESS',     'ONE_TIME',         -1)," +
+                "(12,   'PROGRAMMED',       'Mock Task 12',     'Task 12 - One-time Reminder',      'PERSONAL',     'ONE_TIME',         -1)," +
+                "(13,   'PROGRAMMED',       'Mock Task 13',     'Task 13 - One-time Reminder',      'BUSINESS',     'ONE_TIME',         -1)," +
+                "(14,   'PROGRAMMED',       'Mock Task 14',     'Task 14 - One-time Reminder',      'PERSONAL',     'ONE_TIME',         -1)," +
+                "(15,   'PROGRAMMED',       'Mock Task 15',     'Task 15 - One-time Reminder',      'BUSINESS',     'ONE_TIME',         -1)," +
+                "(16,   'PROGRAMMED',       'Mock Task 16',     'Task 16 - Repeating Reminder',     'PERSONAL',     'REPEATING',        -1)," +
+                "(17,   'PROGRAMMED',       'Mock Task 17',     'Task 17 - Repeating Reminder',     'BUSINESS',     'REPEATING',        -1)," +
+                "(18,   'PROGRAMMED',       'Mock Task 18',     'Task 18 - Repeating Reminder',     'PERSONAL',     'REPEATING',        -1)," +
+                "(19,   'PROGRAMMED',       'Mock Task 19',     'Task 19 - Location Reminder',      'BUSINESS',     'LOCATION_BASED',   -1)," +
+                "(20,   'PROGRAMMED',       'Mock Task 20',     'Task 20 - Location Reminder',      'PERSONAL',     'LOCATION_BASED',   -1)," +
+                "(21,   'PROGRAMMED',       'Mock Task 21',     'Task 21 - Location Reminder',      'BUSINESS',     'LOCATION_BASED',   -1)," +
+                "(22,   'PROGRAMMED',       'Mock Task 22',     'Task 22 - Location Reminder',      'PERSONAL',     'LOCATION_BASED',   -1);";
         sqLiteDatabase.execSQL(statement);
 
 
@@ -183,11 +212,19 @@ public class RemindyDbHelper extends SQLiteOpenHelper {
                 RemindyContract.OneTimeReminderTable.COLUMN_NAME_DATE.getName() + COMMA_SEP +
                 RemindyContract.OneTimeReminderTable.COLUMN_NAME_TIME.getName() +
                 ") VALUES " +
-                "(0, 4, "+dateToday+", "+time1930+")," +
-                "(1, 5, "+dateTomorrow+", "+time0600+")," +
-                "(2, 11, "+dateLastWeek+", "+time1259+")," +
-                "(3, 12, "+dateYesterday+", "+time0600+")," +
-                "(4, 13, "+dateYesterday+", "+time1800+");";
+                "(0,    4,      "+dateToday+",           "+time0600+")," +
+                "(1,    5,      "+dateTomorrow+",        "+time1259+")," +
+                "(2,    6,      "+dateNextWeek+",        "+time1930+")," +
+                "(3,    7,      "+dateNextMonth+",       "+time0600+")," +
+                "(4,    8,      "+dateNext3Months+",     "+time1800+")," +
+                "(5,    9,      "+dateNextYear+",        "+time0600+")," +
+
+                "(6,    10,     "+dateToday+",          "+time1259+")," +
+                "(7,    11,     "+dateTomorrow+",       "+time1930+")," +
+                "(8,    12,     "+dateIn2Days+",       "+time0600+")," +
+                "(9,    13,     "+dateNextMonth+",      "+time1930+")," +
+                "(10,   14,     "+dateNext3Months+",    "+time0600+")," +
+                "(11,   15,     "+dateFuture+",       "+time1800+");";
         sqLiteDatabase.execSQL(statement);
 
 
@@ -204,9 +241,9 @@ public class RemindyDbHelper extends SQLiteOpenHelper {
                 RemindyContract.RepeatingReminderTable.COLUMN_NAME_REPEAT_END_NUMBER_OF_EVENTS.getName() + COMMA_SEP +
                 RemindyContract.RepeatingReminderTable.COLUMN_NAME_REPEAT_END_DATE.getName() +
                 ") VALUES " +
-                "(0, 6, "+dateToday+", "+time0600+", 'DAILY', 2, 'FOREVER', -1, -1)," +
-                "(1, 9, "+dateLastWeek+", "+time1800+", 'MONTHLY', 2, 'UNTIL_DATE', -1, "+dateNextWeek+")," +
-                "(2, 10, "+dateYesterday+", "+time1930+", 'WEEKLY', 1, 'FOR_X_EVENTS', 2, -1);";
+                "(0,    16,     "+dateYesterday+",  "+time0600+", 'MONTHLY',    2,      'FOREVER',          -1,     -1)," +
+                "(1,    17,     "+dateNextWeek+",   "+time1800+", 'DAILY',      2,      'UNTIL_DATE',       -1,     "+dateNextWeek+")," +
+                "(2,    18,     "+dateNextYear+",   "+time1930+", 'WEEKLY',     1,      'FOR_X_EVENTS',     2,      -1);";
         sqLiteDatabase.execSQL(statement);
 
 
@@ -236,8 +273,10 @@ public class RemindyDbHelper extends SQLiteOpenHelper {
                 RemindyContract.LocationBasedReminderTable.COLUMN_NAME_PLACE_FK.getName() + COMMA_SEP +
                 RemindyContract.LocationBasedReminderTable.COLUMN_NAME_IS_ENTERING.getName() +
                 ") VALUES " +
-                "(0, 7, 0, 'TRUE')," +
-                "(1, 8, 2, 'FALSE');";
+                "(0, 19, 0, 'TRUE')," +
+                "(1, 20, 1, 'TRUE')," +
+                "(2, 21, 2, 'TRUE')," +
+                "(3, 22, 3, 'FALSE');";
         sqLiteDatabase.execSQL(statement);
 
     }
