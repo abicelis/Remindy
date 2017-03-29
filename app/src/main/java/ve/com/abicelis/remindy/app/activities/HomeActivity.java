57ee6e2027264250c5bc0e5dc3a2c0e3de56695b
abicelis <abicelis@gmail.com>
2017-03-29 00:40:08 -0400

    Removed onCreateOptionsMenu(), ViewPager Fragments handle this.

--- app/src/main/java/ve/com/abicelis/remindy/app/activities/HomeActivity.java
+++ app/src/main/java/ve/com/abicelis/remindy/app/activities/HomeActivity.java
@@ -67,13 +67,6 @@ public class HomeActivity extends AppCompatActivity implements View.OnClickListe
     }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_home_programmed, menu);
-        return true;
-    }
-
-    @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         int id = item.getItemId();
 
