82f65bc115a83de8c1bf94db7c6f0989a4c9619a
abicelis <abicelis@gmail.com>
2017-04-24 10:15:14 -0400

    Properly implemented comparator for repeating reminders

--- app/src/main/java/ve/com/abicelis/remindy/model/TasksByReminderDateComparator.java
+++ app/src/main/java/ve/com/abicelis/remindy/model/TasksByReminderDateComparator.java
@@ -7,6 +7,7 @@ import ve.com.abicelis.remindy.enums.ReminderType;
 import ve.com.abicelis.remindy.model.Task;
 import ve.com.abicelis.remindy.model.reminder.OneTimeReminder;
 import ve.com.abicelis.remindy.model.reminder.RepeatingReminder;
+import ve.com.abicelis.remindy.util.TaskUtil;
 
 /**
  * Created by abice on 11/3/2017.
@@ -20,8 +21,25 @@ public class TasksByReminderDateComparator implements Comparator<Task> {
         if(o2.getReminder() == null || (o2.getReminderType() != ReminderType.ONE_TIME && o2.getReminderType() != ReminderType.REPEATING))
             return -1;
 
-        Calendar o1Date = (o1.getReminderType() == ReminderType.ONE_TIME ? ((OneTimeReminder) o1.getReminder()).getDate() : ((RepeatingReminder) o1.getReminder()).getDate() );
-        Calendar o2Date = (o2.getReminderType() == ReminderType.ONE_TIME ? ((OneTimeReminder) o2.getReminder()).getDate() : ((RepeatingReminder) o2.getReminder()).getDate() );
+
+        Calendar o1Date = null;
+        if(o1.getReminderType() == ReminderType.ONE_TIME)
+            o1Date = ((OneTimeReminder) o1.getReminder()).getDate();
+        else {  //Repeating reminder
+            Calendar cal = TaskUtil.getRepeatingReminderNextDate(((RepeatingReminder) o1.getReminder()));
+            o1Date = (cal != null ? cal : TaskUtil.getRepeatingReminderEndDate(((RepeatingReminder) o1.getReminder())));
+        }
+
+        Calendar o2Date = null;
+        if(o2.getReminderType() == ReminderType.ONE_TIME)
+            o2Date = ((OneTimeReminder) o2.getReminder()).getDate();
+        else {  //Repeating reminder
+            Calendar cal = TaskUtil.getRepeatingReminderNextDate(((RepeatingReminder) o2.getReminder()));
+            o1Date = (cal != null ? cal : TaskUtil.getRepeatingReminderEndDate(((RepeatingReminder) o2.getReminder())));
+        }
+
+        //Calendar o1Date = (o1.getReminderType() == ReminderType.ONE_TIME ? ((OneTimeReminder) o1.getReminder()).getDate() : TaskUtil.getRepeatingReminderNextDate(((RepeatingReminder) o1.getReminder())) );
+        //Calendar o2Date = (o2.getReminderType() == ReminderType.ONE_TIME ? ((OneTimeReminder) o2.getReminder()).getDate() : TaskUtil.getRepeatingReminderNextDate(((RepeatingReminder) o2.getReminder())) );
         return o1Date.compareTo(o2Date);
     }
 }
