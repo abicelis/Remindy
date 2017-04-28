196363695b0b3250a68c83df6c42d9fe226be0ad
abicelis <abicelis@gmail.com>
2017-04-27 20:57:16 -0400

    Added Toast message to copyText()

--- app/src/main/java/ve/com/abicelis/remindy/util/ClipboardUtil.java
+++ app/src/main/java/ve/com/abicelis/remindy/util/ClipboardUtil.java
@@ -4,6 +4,9 @@ import android.app.Activity;
 import android.content.ClipData;
 import android.content.ClipboardManager;
 import android.content.Context;
+import android.widget.Toast;
+
+import ve.com.abicelis.remindy.R;
 
 /**
  * Created by abice on 16/4/2017.
@@ -15,5 +18,7 @@ public class ClipboardUtil {
         ClipboardManager clipboard = (ClipboardManager) activity.getSystemService(Context.CLIPBOARD_SERVICE);
         ClipData clip = ClipData.newPlainText("Remindy text", textToCopy);
         clipboard.setPrimaryClip(clip);
+        Toast.makeText(activity, R.string.dialog_text_attachment_text_copied_clipboard, Toast.LENGTH_SHORT).show();
+
     }
 }
