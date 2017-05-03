9e5430ef5bda10b2e69ee244e1bfa714abda3051
abicelis <abicelis@gmail.com>
2017-05-03 11:27:26 -0400

    getTriggerDateWithTime() implementation

--- app/src/main/java/ve/com/abicelis/remindy/viewmodel/TaskTriggerViewModel.java
+++ app/src/main/java/ve/com/abicelis/remindy/viewmodel/TaskTriggerViewModel.java
@@ -8,6 +8,7 @@ import java.util.Calendar;
 import ve.com.abicelis.remindy.enums.TaskViewModelType;
 import ve.com.abicelis.remindy.model.Task;
 import ve.com.abicelis.remindy.model.Time;
+import ve.com.abicelis.remindy.util.CalendarUtil;
 
 /**
  * Created by abice on 26/3/2017.
@@ -29,4 +30,14 @@ public class TaskTriggerViewModel {
     public Task getTask() { return task; }
     public Calendar getTriggerDate() { return triggerDate; }
     public Time getTriggerTime() { return triggerTime; }
+
+    public Calendar getTriggerDateWithTime() {
+        Calendar cal = CalendarUtil.getNewInstanceZeroedCalendar();
+        cal.setTimeInMillis(getTriggerDate().getTimeInMillis());
+
+        cal.set(Calendar.HOUR_OF_DAY, getTriggerTime().getHour());
+        cal.set(Calendar.MINUTE, getTriggerTime().getMinute());
+
+        return cal;
+    }
 }
