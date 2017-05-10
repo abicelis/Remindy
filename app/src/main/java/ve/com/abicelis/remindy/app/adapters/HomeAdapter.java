eda3c44e384d75a3f12af171c7884b6070a1622c
abicelis <abicelis@gmail.com>
2017-05-09 22:37:23 -0400

    New: Added item remove functions

--- app/src/main/java/ve/com/abicelis/remindy/app/adapters/HomeAdapter.java
+++ app/src/main/java/ve/com/abicelis/remindy/app/adapters/HomeAdapter.java
@@ -8,6 +8,8 @@ import android.view.ViewGroup;
 
 import java.security.InvalidParameterException;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 import ve.com.abicelis.remindy.R;
@@ -112,5 +114,51 @@ public class HomeAdapter extends SelectableAdapter<RecyclerView.ViewHolder> {
     @Override
     public int getItemCount() {
         return mTasks.size();
+    }
+
+
+    public void removeItem(int position) {
+        mTasks.remove(position);
+        notifyItemRemoved(position);
+    }
+
+    public void removeItems(List<Integer> positions) {
+        // Reverse-sort the list
+        Collections.sort(positions, new Comparator<Integer>() {
+            @Override
+            public int compare(Integer lhs, Integer rhs) {
+                return rhs - lhs;
+            }
+        });
+
+        // Split the list in ranges
+        while (!positions.isEmpty()) {
+            if (positions.size() == 1) {
+                removeItem(positions.get(0));
+                positions.remove(0);
+            } else {
+                int count = 1;
+                while (positions.size() > count && positions.get(count).equals(positions.get(count - 1) - 1)) {
+                    ++count;
+                }
+
+                if (count == 1) {
+                    removeItem(positions.get(0));
+                } else {
+                    removeRange(positions.get(count - 1), count);
+                }
+
+                for (int i = 0; i < count; ++i) {
+                    positions.remove(0);
+                }
+            }
+        }
+    }
+
+    private void removeRange(int positionStart, int itemCount) {
+        for (int i = 0; i < itemCount; ++i) {
+            mTasks.remove(positionStart);
+        }
+        notifyItemRangeRemoved(positionStart, itemCount);
     }
 }
